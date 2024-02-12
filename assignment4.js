bc59d9278aa55feb55b70af085e5b21d7929b3ef
Rupak Samanta <rupaksamantainbox@gmail.com>
2024-02-12 22:40:36 +0530

    Assignment 4 Done

--- assignment4.js
+++ assignment4.js
@@ -1,130 +1,130 @@
 //Question 1
 
-// function Counter(){
-//     let count = 0
+function Counter(){
+    let count = 0
 
-//     return function(){
-//         count ++
-//         console.log("Count :"+count)
-//     }
-// }
+    return function(){
+        count ++
+        console.log("Count :"+count)
+    }
+}
 
-// let myCounter = Counter()
-// myCounter()
-// myCounter()
-// myCounter()
+let myCounter = Counter()
+myCounter()
+myCounter()
+myCounter()
 
 
 //Question 2
 
-// const order = {
-//     orderId : '1234',
-//     productName : 'I phone 15',
-//     quantity: 1
-// }
+const order = {
+    orderId : '1234',
+    productName : 'I phone 15',
+    quantity: 1
+}
 
-// let {orderId,productName,quantity} = order
+let {orderId,productName,quantity} = order
 
-// console.log(orderId)
-// console.log(productName)
-// console.log(quantity)
+console.log(orderId)
+console.log(productName)
+console.log(quantity)
 
 
-//Question 3 
+Question 3 
 
-// function Cart(){
-//     const items = ['Iphone 15','laptop','tshirt'];
-//     return {
-//             getCartItems : function(){
-//             return items;
-//         }
-//     }
-// }
-// const myCart = Cart();
-// console.log("Cart Items :",myCart.getCartItems())
+function Cart(){
+    const items = ['Iphone 15','laptop','tshirt'];
+    return {
+            getCartItems : function(){
+            return items;
+        }
+    }
+}
+const myCart = Cart();
+console.log("Cart Items :",myCart.getCartItems())
 
 
 //Question 4 
 
-// function shoppingCart(){
+function shoppingCart(){
 
-//     const cartItems = [];
-//     function itemIndexInCart(productID){
-//         return cartItems.findIndex(item => item.id === productID);
-//     }
-//     return{
-//         addItem : function(product){
-//             const itemIndex = itemIndexInCart(product.id);
+    const cartItems = [];
+    function itemIndexInCart(productID){
+        return cartItems.findIndex(item => item.id === productID);
+    }
+    return{
+        addItem : function(product){
+            const itemIndex = itemIndexInCart(product.id);
 
-//             if(itemIndex !== -1){
-//                 cartItems[itemIndex].quantity++;
-//             }else{
-//                 cartItems.push({id:product.id,name:product.name,price:product.price,quantity:1});
-//             }
-//         },
-//         getCartItems : function(){
-//             return cartItems;
-//         }
-//     };   
-// }
+            if(itemIndex !== -1){
+                cartItems[itemIndex].quantity++;
+            }else{
+                cartItems.push({id:product.id,name:product.name,price:product.price,quantity:1});
+            }
+        },
+        getCartItems : function(){
+            return cartItems;
+        }
+    };   
+}
 
-// const cart = shoppingCart();
-// console.log("Cart Items :",cart.getCartItems())
+const cart = shoppingCart();
+console.log("Cart Items :",cart.getCartItems())
 
-// const product1 = {id:1, name:"Iphone 15 pro max", price:150000}
-// const product2 = {id:2, name:"S24 Ultra", price:159000}
+const product1 = {id:1, name:"Iphone 15 pro max", price:150000}
+const product2 = {id:2, name:"S24 Ultra", price:159000}
 
-// cart.addItem(product1)
-// cart.addItem(product2)
+cart.addItem(product1)
+cart.addItem(product2)
 
-// console.log("Cart Items :",cart.getCartItems())
+console.log("Cart Items :",cart.getCartItems())
 
 
 //Question 5
 
-// function shoppingCart(){
+function shoppingCart(){
 
-//     const cartItems = [];
-//     function itemIndexInCart(productID){
-//         return cartItems.findIndex(item => item.id === productID);
-//     }
-//     return{
-//         addItem : function(product){
-//             const itemIndex = itemIndexInCart(product.id);
+    const cartItems = [];
+    function itemIndexInCart(productID){
+        return cartItems.findIndex(item => item.id === productID);
+    }
+    return{
+        addItem : function(product){
+            const itemIndex = itemIndexInCart(product.id);
 
-//             if(itemIndex !== -1){
-//                 cartItems[itemIndex].quantity++;
-//             }else{
-//                 cartItems.push({id:product.id,name:product.name,price:product.price,quantity:1});
-//             }
-//         },
-//         getCartItems : function(){
-//             return cartItems;
-//         },
-//         removeItem : function(productId){
-//             const itemIndex = itemIndexInCart(productId);
+            if(itemIndex !== -1){
+                cartItems[itemIndex].quantity++;
+            }else{
+                cartItems.push({id:product.id,name:product.name,price:product.price,quantity:1});
+            }
+        },
+        getCartItems : function(){
+            return cartItems;
+        },
+        removeItem : function(productId){
+            const itemIndex = itemIndexInCart(productId);
 
-//             if(itemIndex !== -1){
-//                 cartItems.splice(itemIndex,1)
-//             }
-//         }
-//     };   
-// }
+            if(itemIndex !== -1){
+                cartItems.splice(itemIndex,1)
+            }
+        }
+    };   
+}
 
-// const cart = shoppingCart();
-// console.log("Cart Items :",cart.getCartItems())
+const cart = shoppingCart();
+console.log("Cart Items :",cart.getCartItems())
 
-// const product1 = {id:1, name:"Iphone 15 pro max", price:150000}
-// const product2 = {id:2, name:"S24 Ultra", price:159000}
+const product1 = {id:1, name:"Iphone 15 pro max", price:150000}
+const product2 = {id:2, name:"S24 Ultra", price:159000}
 
-// cart.addItem(product1)
-// cart.addItem(product2)
+cart.addItem(product1)
+cart.addItem(product2)
 
-// console.log("Cart Items :",cart.getCartItems())
+console.log("Cart Items :",cart.getCartItems())
 
-// cart.removeItem(2)
+cart.removeItem(2)
 
-// console.log("Cart Items :",cart.getCartItems())
+console.log("Cart Items :",cart.getCartItems())
 
 
 //Question 6
